b50867ab2a5c10fec1c778280a494da68153a126
Samir Mathrani <Samir@dyn-160-39-162-114.dyn.columbia.edu>
2014-02-07 00:54:42 -0500

    Added QR scanner example code.

---
+++ ZBarAndroidSDK-0.1/examples/CameraTest/bin/classes.dex.d
@@ -0,0 +1,14 @@
+/Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/bin/classes.dex : \
+/Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/bin/classes/net/sourceforge/zbar/android/CameraTest/BuildConfig.class \
+/Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/bin/classes/net/sourceforge/zbar/android/CameraTest/CameraPreview.class \
+/Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/bin/classes/net/sourceforge/zbar/android/CameraTest/CameraTestActivity$1.class \
+/Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/bin/classes/net/sourceforge/zbar/android/CameraTest/CameraTestActivity$2.class \
+/Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/bin/classes/net/sourceforge/zbar/android/CameraTest/CameraTestActivity$3.class \
+/Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/bin/classes/net/sourceforge/zbar/android/CameraTest/CameraTestActivity$4.class \
+/Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/bin/classes/net/sourceforge/zbar/android/CameraTest/CameraTestActivity.class \
+/Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/bin/classes/net/sourceforge/zbar/android/CameraTest/R$attr.class \
+/Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/bin/classes/net/sourceforge/zbar/android/CameraTest/R$id.class \
+/Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/bin/classes/net/sourceforge/zbar/android/CameraTest/R$layout.class \
+/Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/bin/classes/net/sourceforge/zbar/android/CameraTest/R$string.class \
+/Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/bin/classes/net/sourceforge/zbar/android/CameraTest/R.class \
+/Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/bin/dexedLibs/zbar-f579e4aee970f07668e7e66e907f825e.jar \
---
+++ ZBarAndroidSDK-0.1/examples/CameraTest/gen/R.java.d
@@ -0,0 +1,4 @@
+/Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/gen/net/sourceforge/zbar/android/CameraTest/R.java \
+ : /Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/res/layout/main.xml \
+/Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/res/values/strings.xml \
+/Users/Samir/Documents/workspace/ZBarAndroidSDK-0.1/examples/CameraTest/bin/AndroidManifest.xml \
