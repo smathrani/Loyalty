1e1d232c4dc2c9c928304cd6b8918f994ad0c01b
Amar Dhingra <Amar@dyn-160-39-196-233.dyn.columbia.edu>
2014-02-09 21:18:06 -0500

    Card with buttons

--- Card.java
+++ Card.java
@@ -1,11 +1,11 @@
-package com.example.cards;
+package com.example.loyalty;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
@@ -25,7 +25,7 @@ public class Card extends TableLayout {
 	//private LinearLayout.LayoutParams fourVertical = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 0, 4f);
 	
 	private LinearLayout.LayoutParams oneHorizontal = new LinearLayout.LayoutParams(0, LayoutParams.MATCH_PARENT, 1f);
-	//private LinearLayout.LayoutParams twoHorizontal = new LinearLayout.LayoutParams(0, LayoutParams.MATCH_PARENT, 2f);
+	private LinearLayout.LayoutParams twoHorizontal = new LinearLayout.LayoutParams(0, LayoutParams.MATCH_PARENT, 2f);
 	private LinearLayout.LayoutParams threeHorizontal = new LinearLayout.LayoutParams(0, LayoutParams.MATCH_PARENT, 3f);
 	//private LinearLayout.LayoutParams fourHorizontal = new LinearLayout.LayoutParams(0, LayoutParams.MATCH_PARENT, 4f);
 
@@ -57,9 +57,13 @@ public class Card extends TableLayout {
 		top.setOrientation(TableLayout.HORIZONTAL);
 		top.setBackgroundColor(Color.WHITE);
 		top.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, 0, 1f));
+		top.setPadding(height/100,height/100,height/100, height/100);
 		
 			ImageView logo = new ImageView(getContext());
-			logo.setBackgroundResource(R.drawable.a);
+			logo.setImageResource(R.drawable.camera);
+			int factor = 7;
+			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(height/factor, height/factor);
+			logo.setLayoutParams(params);
 			
 			TableLayout thirdTable = new TableLayout(getContext());
 			thirdTable.setOrientation(TableLayout.VERTICAL);
@@ -68,10 +72,11 @@ public class Card extends TableLayout {
 				TextView nametv = new TextView(getContext());
 				nametv.setText(name);
 				nametv.setTextSize(30);
+				nametv.setGravity(Gravity.CENTER_HORIZONTAL);
 				
 				TextView pointstv = new TextView(getContext());
 				pointstv.setText(points);
-				pointstv.setTextSize(80);
+				pointstv.setTextSize(30);
 				pointstv.setGravity(Gravity.CENTER);
 				
 				thirdTable.addView(nametv);
@@ -79,26 +84,63 @@ public class Card extends TableLayout {
 			
 			top.addView(logo);
 			top.addView(thirdTable);
+			top.setLayoutParams(margin);
 		
-		LinearLayout bottom = new LinearLayout(getContext());
+		TableLayout bottom = new TableLayout(getContext());
 		bottom.setOrientation(TableLayout.VERTICAL);
-		bottom.setBackgroundColor(Color.GRAY);
+		//bottom.setBackgroundColor(Color.GRAY);
 		bottom.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, 0, 1f));
 		
 			LayoutParams topMargin = new LayoutParams();
 			topMargin.setMargins(0, 2, 0, 2);
+		
+		
+			TableRow row1 = new TableRow(getContext());
 			
-			final TextView row1 = new TextView(getContext());
-			setupRow(row1, "row 1");
+				TextView row1tv = new TextView(getContext());
+				setupTextView(row1tv, "row 1");
+				
+				Button row1button = new Button(getContext());
+				setupButton(row1button, "button");
+				
+			row1.addView(row1tv);
+			row1.addView(row1button);
 			
-			TextView row2 = new TextView(getContext());
-			setupRow(row2, "row 2");
-			
-			TextView row3 = new TextView(getContext());
-			setupRow(row3, "row 3");
 
-			TextView row4 = new TextView(getContext());
-			setupRow(row4, "row 4");
+			TableRow row2 = new TableRow(getContext());
+			
+				TextView row2tv = new TextView(getContext());
+				setupTextView(row2tv, "row 2");
+				
+				Button row2button = new Button(getContext());
+				setupButton(row1button, "button");
+				
+			row2.addView(row2tv);
+			row2.addView(row2button);
+			
+
+			TableRow row3 = new TableRow(getContext());
+			
+				TextView row3tv = new TextView(getContext());
+				setupTextView(row3tv, "row 3");
+				
+				Button row3button = new Button(getContext());
+				setupButton(row3button, "button");
+				
+			row3.addView(row3tv);
+			row3.addView(row3button);
+			
+
+			TableRow row4 = new TableRow(getContext());
+			
+				TextView row4tv = new TextView(getContext());
+				setupTextView(row4tv, "row 4");
+				
+				Button row4button = new Button(getContext());
+				setupButton(row4button, "button");
+				
+			row4.addView(row4tv);
+			row4.addView(row4button);
 
 			
 			bottom.addView(row1);
@@ -129,7 +171,14 @@ public class Card extends TableLayout {
 		
 	}
 	
-	private void setupRow(TextView row, String data)
+	private void setupButton(Button button, String data) {
+		button.setText(data);
+		button.setBackgroundColor(Color.WHITE);
+		button.setLayoutParams(margin);
+		button.setTextSize(20);		
+	}
+
+	private void setupTextView(TextView row, String data)
 	{
 		row.setText(data);
 		row.setBackgroundColor(Color.WHITE);
@@ -148,10 +197,17 @@ public class Card extends TableLayout {
 
 		LinearLayout table = new LinearLayout(getContext());
 		table.setOrientation(LinearLayout.HORIZONTAL);
+		table.setBackgroundColor(Color.WHITE);
+
+
 
 			ImageView logo = new ImageView(getContext());
-			logo.setBackgroundResource(R.drawable.camera);
-			logo.setLayoutParams(oneHorizontal);
+			logo.setImageResource(R.drawable.camera);
+			int factor = 12;
+			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(height/factor,height/factor);
+			params.setMargins(height/200, height/200, height/200, height/200);
+			logo.setLayoutParams(params);
+
 			
 			TextView nametv = new TextView(getContext());
 			nametv.setText(name);
